2aad63fce147269f0daba5056686fe83c60984ab
Rahul gupta <rahul26021999@gmail.com>
2019-06-13 00:08:28 +0530

    task3 completed

--- app/Http/Controllers/WebServicesController.php
+++ app/Http/Controllers/WebServicesController.php
@@ -16,7 +16,7 @@ class WebServicesController extends Controller
 
 		$validator=$this->check($request);
 		if ($validator->fails()) {
-			return response()->json([$validator->errors(),"status"=>"fail|0"],422);	
+			return response()->json(["errors"=>$validator->errors(),"status"=>"fail|0"],422);	
 		}
 		else{
 			$this->submit($request);
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,72 +1,91 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Form Validation</title>
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
-
-    <style>
-        .bigFont{
-            font-size:1.5rem;
-            margin-left: 5px;
-            color: grey;
-        }
-    </style>
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<title>Form Validation</title>
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
 </head>
 <body>
-    <h1 class="text-center my-5 text-success">Task 1 : Form Validation</h1>
+	<h2 class="text-center my-5 text-success">Task 3 : Form Validation through AJAX</h2>
 
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+	<!-- Optional JavaScript -->
+	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
 
+	<script>
+		function closeBox()
+		{
+			$("#errorBox").addClass("d-none");
+			$("#successBox").addClass("d-none");
+		}
+		function checkAndSubmit()
+		{	
+			closeBox();
+			var name = $('#name').val();
+			var email = $('#email').val();
+			var pincode = $('#pincode').val();
+			var send="email="+email+"&name="+name+"&pincode="+pincode;
+			var xhttp = new XMLHttpRequest();
+			xhttp.onreadystatechange = function() {
+				if (this.readyState == 4 && this.status == 200) {
+					$("#successBox").removeClass("d-none");
 
+					$('#name').val("");
+					$('#email').val("");
+					$('#pincode').val("");
+				}
+				else if(this.readyState == 4 && this.status == 422)
+				{
+					$("#errorBox").removeClass("d-none");
+					var myObj = JSON.parse(this.responseText);
+					var output="";
+					for (var property in myObj['errors']) {
+						output += "<div>"+myObj['errors'][property]+"</div>";
+					}
+					$('#error').html(output);
+				}
+			};
+			xhttp.open("POST", "http://127.0.0.1:8000/api/register", true);
+			xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+			xhttp.send(send);
+		}
 
-    <div class="container">
+	</script>
+	<div class="container">
+		
+		<div class="alert alert-success d-none" id="successBox">
+			<button type="button" class="close" onclick="closeBox()">&times;</button>
+			<strong>successfull submitted !!</strong> .
+		</div>
+		<div class="alert alert-danger d-none" id="errorBox">
+			<button type="button" class="close" onclick="closeBox()">&times;</button>
+			<div id="error"></div>
+		</div>
 
-      @isset($success)
-        <div class="alert alert-success alert-dismissible">
-          <button type="button" class="close" data-dismiss="alert">&times;</button>
-          <strong>successfull submitted !!</strong> .
-      </div>
-      @endisset
+		<form>
+			@csrf()
+			<div class="form-group">
+				<label for="email" class="font-weight-bold">Email</label>
+				<input type="text" class="form-control" id="email" name="email" placeholder="Enter your mail" autofocus>
+			</div>
+			<div class="form-group">
+				<label for="name" class="font-weight-bold">Name</label>
+				<input type="text" class="form-control" id="name"  name="name" placeholder="Enter your name">
+			</div>
+			<div class="form-group">
+				<label for="pincode" class="font-weight-bold">Pincode</label>
+				<input type="number" class="form-control" id="pincode" name="pincode" placeholder="Pincode">
+			</div>
+			<div class="text-center">
+				<input type="button" name="submit" value="submit" class="btn btn-info btn-lg text-uppercase" onclick="checkAndSubmit()">
+			</div>
+		</form>
 
-      @if($errors->any())
-      <div>
-        @foreach($errors->all() as $error)
-        <div class="alert alert-danger alert-dismissible">
-          <button type="button" class="close" data-dismiss="alert">&times;</button>
-          <strong>Error! </strong>{{$error}} .
-      </div>
-      @endforeach
-  </div>
-  @endif
-
-  <form action="/" method="POST">
-    @csrf()
-    <div class="form-group">
-        <label for="email" class="font-weight-bold bigFont">Email</label>
-        <input type="text" class="form-control form-control-lg" id="email" name="email" placeholder="Enter your mail" autofocus value="{{old('email')}}"  >
-    </div>
-
-    <div class="form-group">
-        <label for="name" class="font-weight-bold bigFont">Name</label>
-        <input type="text" class="form-control form-control-lg" id="name"  name="name" placeholder="Enter your name" value="{{old('name')}}" pattern="[A-Za-z]+">
-    </div>
-    <div class="form-group">
-        <label for="pincode" class="font-weight-bold bigFont">Pincode</label>
-        <input type="number" class="form-control form-control-lg" id="pincode" name="pincode" placeholder="Pincode" value="{{old('pincode')}}"  minlength="6" maxlength="6">
-    </div>
-    <div class="text-center">
-        <input type="submit" name="submit" value="submit" class="btn btn-info btn-lg text-uppercase">
-    </div>
-</form>
-
-</div>
+	</div>
 </body>
 </html>
